1ee2ee6a5e19f1ee387aa911c4da1ec05cf9c49d
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2020-09-30 14:17:09 -0400

    Add files via upload

---
+++ argonnx.php
@@ -0,0 +1,22 @@
+<?php
+$output = shell_exec('TegraRcmSmash.exe "argonnx.bin"');
+echo "<html>\n";
+echo "<style>\n";
+echo "html {\n";
+echo "  background-color: #2d2d2d;\n";
+echo "  color: #ffffff;\n";
+echo "  text-decoration: none;\n";
+echo "}\n";
+echo ".inner {\n";
+echo "  display: inline-block;\n";
+echo "}\n";
+echo ".right {\n";
+echo "  float: right;\n";
+echo "}\n";
+echo "</style>\n";
+echo "<div>\n";
+echo "<a class=\"inner\" href=\"index.html\"><img src=\"saturnNX-dark_icon.png\" width=\"100px\"></a>\n";
+echo "<a class=\"inner right\"><img src=\"icon_argonnx-alt.png\" width=\"100px\"></a>\n";
+echo "<br>";
+echo "<pre>$output</pre>";
+?>
